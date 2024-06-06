5241ddbfb8d812f0b75e67c718e47586b664b556
gruszczrob <=>
2024-06-06 15:57:31 +0200

    Implementation of Cli

---
+++ Cli/Cli.h
@@ -0,0 +1,25 @@
+#ifndef CLI_H
+#define CLI_H
+
+#include <iostream>
+#include <any>
+#include <memory>
+#include <vector>
+#include "../Logs/Logs.h"
+#include "../DrawingEngine/DrawingEngine.h"
+
+using namespace std;
+
+class Cli : public DrawingEngine {
+private:
+    shared_ptr<char> wall = make_shared<char>();
+    shared_ptr<char> route = make_shared<char>();
+
+public:
+    Cli(any widthI, any heightI, any wallI, any routeI);
+
+    void print(vector<vector<int>> grid);
+};
+
+#endif /* CLI_H */
+
